69c0a1228a0ce27ae272be2aaa28486a7fb0f4c2
Farjana Huq <fa.huq03@gmail.com>
2021-07-12 11:39:14 -0700

    nav bar initial feature complete

--- project-navbar/src/components/Navbar.vue
+++ project-navbar/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid" style="background-color:blue height: 250px width: 100%">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Logo</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -42,23 +42,28 @@
                 </div>
             </li>
             </ul>
-            <ul class="navbar-nav mr-auto" style="inline-block">
-              <li class="nav-item dropdown list-unstyled">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">0XF696...6EDE
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <div>
-                      <a class="dropdown-item" href="#">Action</a>
-                    </div>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-               </li>
-               <li class="list-unstyled" style="color:gray font-weight:light"> | </li>
-               <div></div>
-               <li class="list-unstyled"> <i class="fa fa-cog" ></i></li>
-            </ul>        
+            <div class="w3-bar">     
+              <ul class="navbar-nav mr-auto list-unstyled justify-content-end">
+                <li class="nav-item dropdown ">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">0XF696...6EDE
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <div>
+                        <a class="dropdown-item" href="#">Your Wallets</a>
+                      </div>
+                          <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Wallet 1</a>
+                  
+                      <a class="dropdown-item" href="#">Wallet 2 </a>
+                  </div>
+                </li>
+                <li style="color:gray font-weight:light padding-right: 2px"> | </li>
+                
+                <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-cog" ></i></a>
+                   
+                </li>
+              </ul> 
+            </div>       
         </div>
         </nav>
     </div>
@@ -84,21 +89,18 @@ body {
   color: #000;
   font-weight: bold;
 }
-.container {
-  max-width: 100%;
-  margin: 0px auto;
-  overflow: auto;
-  max-height: 100px;
-  /* border: 1px solid steelblue; */
-  /* padding: 5px; */
-  background-color: #000;
-}
 .navbar{
      max-width: 90%;
      max-height:80px;
      margin: 20px;
      color: #000;
+     background-color: fff;
      font-weight: bold;
+     padding: 10px;
+     box-shadow: 0.5px 5px ;
+}
+.navbar-toggler{
+  background-color: blue !important;
 }
 .btn {
   display: inline-block;
