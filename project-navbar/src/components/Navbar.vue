7aad006ffafec6862a46665080e4bbdc479cc52b
Farjana Huq <fa.huq03@gmail.com>
2021-07-16 11:05:27 -0700

    navbar fixes

--- project-navbar/src/components/Navbar.vue
+++ project-navbar/src/components/Navbar.vue
@@ -5,7 +5,7 @@
     style="background-color:blue height: 250px width: 100%"
   >
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand Codex-Logo" href="#"><img src="/images/codex-logo.png" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="../assets/images/codex-logo.png" class="Codex-Logo" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -32,7 +32,7 @@
           <li class="nav-item" id="navbar-options">
             <a class="nav-link" href="#" style="width: 60px;height: 23px; margin: 0 16px;">Markets</a>
           </li>
-          <li class="nav-item dropdown" id="navbar-options" style="width: 39px; height: 23px; margin: 0 8px 0 0;">
+          <li class="nav-item dropdown" id="navbar-options" style="padding-left:20px">
             <a
               class="nav-link dropdown-toggle"
               href="#"
@@ -41,10 +41,13 @@
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
+              
             >
               More
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <!-- <i class="fa fa-caret-down" aria-hidden="true"
+             style="width: 12px;height:12px; margin: 6px 0 5px 8px;padding: 3px 0 2px;"></i> -->
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown" >
               <a class="dropdown-item" href="#">Action</a>
               <a class="dropdown-item" href="#">Another action</a>
               <div class="dropdown-divider"></div>
@@ -53,11 +56,13 @@
           </li>
         </ul>
         <!-- Navbar right elements -->
-        <div class="w3-bar">
-          <ul class="navbar-nav mr-auto list-unstyled justify-content-end">
+        <div class="-Components-Navigation-X-Wallet-Connected">
+          <ul class="navbar-nav mr-auto list-unstyled">
             <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
+              <div>
+                <a
+                class="nav-link"
+                style="width: 103px; height:17px; margin: 0 10px 0 0;font-size:14px; font-weight:bold;"
                 href="#"
                 id="navbarDropdown"
                 role="button"
@@ -65,11 +70,13 @@
                 aria-haspopup="true"
                 aria-expanded="false"
                 >0XF696...6EDE
-              </a>
+              </a><i class="fa fa-caret-down dropdown -Icons-FontAwesome-Caret-1-Up " aria-hidden="true"></i>
+              </div>
+              
               <!-- User wallet dropdown menue-->
               <div
                 class="dropdown-menu"
-                aria-labelledby="navbarDropdown"
+                aria-labelledby="navbarDrop"
                 id="user-walet-box"
               >
                 <div class="row" id="user-wallet-items" style="">
@@ -105,46 +112,35 @@
                 </div>
               </div>
             </li>
-            <li style="color:gray font-weight:light padding-right: 2px">|</li>
+            <li style="width: 1px;height: 19px; margin: 0 20px; border: 1px solid black;"></li>
 
             <!-- This section covers navbar settinngs. The gear icon is clickable and displays a dropdown menue to edit layout. -->
             <li class="dropdown">
               <a class="dropdown" role="button" data-toggle="dropdown">
                 <i class="fa fa-cog" style="width:15px; height:16px;"></i
               ></a>
-              <!-- <div class="dropdown-menu Rectangle">
-                <div class="Light">Light</div>
-              </div> -->
               <div class="dropdown-menu" id="settings-rectangle">
                           <div id="setting-layout-inside">
                             <div class="row" id="div-divider">
                                <div class="col Light">Light</div>
                                    <div class="col Shape text-right"><i class="fa fa-toggle-on" aria-hidden="true"></i></div> 
                             </div> 
-                            
-                            <!-- <div class="row" >
-                                   <div class="col Light">Light</div>
-                                   <div class="col Shape text-right"><i class="fa fa-toggle-on" aria-hidden="true"></i></div> 
-                            </div>  -->
-                            
-                            
+              
                             <div class="row">
                                    <div class="col-sm-10" style="font-size:13px; color:#6C757D;border-top:53px; margin-top:16px">Layout Settings</div>
                                      
                                    <div class="col-sm-2 text-right"><i class="fa fa-columns" style="color:#6C757D; margin-top:16px" aria-hidden="true"></i></div>
                             </div>
-                            
+                             <!-- edit layout button -->
                             <div class="row" id="layout-edit" style="font-size:13px; height:20px; widhth:62px; font-weight:light; margin-left:9px; margin-top 10.67px;">
                                       <div class="nav-item" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true" style="margin-right:8px width:16px; height: 14.22px"></i>Edit Layout</div>        
                             </div>
-                           
+                              <!-- default layout button -->
                              <div class="row" id="layout-default" style="font-size:13px; height:20px; widhth:62px; font-weight:light; margin-left:9px; margin-top:19.11px;">
                                       <div class="nav-item" role="button"><i class="fa fa-repeat" aria-hidden="true" style="width:16px; height: 16px"></i>Default Layout</div>        
                             </div>
                        </div>
                   </div>
-                  <!-- edit layout button -->
-                   <!-- default layout button -->
             </li>
           </ul>
         </div>
@@ -269,6 +265,19 @@ height: 20px;
   margin: 20px 24px 20px 0;
   object-fit: contain;
 }
+.-Components-Navigation-X-Wallet-Connected {
+  width: 350px;
+  height: 80px;
+  margin: 0 0 0 394px;
+  padding: 31px 0 30px 170px;
+}
+.-Icons-FontAwesome-Caret-1-Up {
+  width: 12px;
+  height: 12px;
+  margin: 3px 0 2px 10px;
+  padding: 3px 0 2px;
+}
+
 </style>
 
 
